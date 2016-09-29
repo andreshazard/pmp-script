72bfaf29d8816f1f6c926c50e2f3f354de2751a4
Andres Hazard <andres.hazard@servicerocket.com>
2016-09-28 21:03:35 -0300

    remove comments

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ except:
 # connecting to the gmail imap server
 m = imaplib.IMAP4_SSL("imap.gmail.com")
 m.login(user, pwd)
-m.select("INBOX")  # mail box where you get the pmp email
+m.select("INBOX")
 resp, items = m.search(None, "SUBJECT", "Second factor password for Password Manager Pro")
 items = items[0].split()
 if items == []:
@@ -23,15 +23,15 @@ if items == []:
 
 for emailid in items:
     resp, data = m.fetch(emailid, "(RFC822)")  # fetching the mail, "`(RFC822)`" means "get the whole stuff", but you can ask for headers only, etc
-    email_body = data[0][1]  # getting the mail content
-    mail = email.message_from_string(email_body)  # parsing the mail content to get a mail object
+    email_body = data[0][1]
+    mail = email.message_from_string(email_body)
     m.store(emailid, '+FLAGS', '\\Deleted')
     m.expunge()  # Delete message once we have the content
 
     # Getting only the password and not the hole message
     for line in email_body.splitlines():
         if "Password Manager Pro is:" in line:
-            pmp_secondary_pwd = re.sub('[^0-9]', '', line)  # Get only the numbers of that line
+            pmp_secondary_pwd = re.sub('[^0-9]', '', line)
             print "Password = " + pmp_pwd
             print "Secondary Password = " + pmp_secondary_pwd
             break
